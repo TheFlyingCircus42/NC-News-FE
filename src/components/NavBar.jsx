6c05a68fcdf2d3242d5207e106231042ebd3f63e
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-25 14:24:06 +0000

    add links and alignment to links in nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,29 @@
 import '../styles/NavBar.css'
-
-import { useState , useEffect } from 'react'
+// import { useState , useEffect } from 'react'
 import { Link } from "react-router-dom"
 
 function NavBar () {
     return(<>
-    <nav className="nav">
-    
-        <p className="Nav-AllTopics">All Topics</p>
+    <nav className="Nav-Bar">
         
-        <Link to="/api/articles">
-        <p className="Nav-AllArticles">All Articles</p>
+        <Link to="/home">
+        <p className="Nav-Bar-Link"> Home </p>
         </Link>
 
-        <p className="Nav-APIInfo">API Info</p>
+        <Link to="/api/topics">
+        <p className="Nav-Bar-Link"> All Topics </p>
+        </Link>
+
+        <Link to="/api/articles">
+        <p className="Nav-Bar-Link"> All Articles </p>
+        </Link>
+
+        <Link to="/api">
+        <p className="Nav-Bar-Link"> API Info </p>
+        </Link>
+
         
-        <p className="Nav-OtherLink">???</p>
+        {/* <p className="Nav-Bar-Link"> ??? </p> */}
 
     </nav>
     
