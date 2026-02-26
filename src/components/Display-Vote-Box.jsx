ad1fcd1611ef7497c22df0e452098ce92a8afd1d
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-26 15:03:09 +0000

    remove voting restriction - temporary

--- src/components/Display-Vote-Box.jsx
+++ src/components/Display-Vote-Box.jsx
@@ -6,11 +6,12 @@ function VoterBox ({article})
 {
     const [voteCount , setVoteCount] = useState(article.votes)
     const [isVoting , setIsVoting] = useState(false)
+    const [hasVoted , setHasVoted] = useState(false)
     const [error , setError] = useState(null)
 
     async function handleVote(change)
     {
-        if (isVoting) return
+        if (isVoting || hasVoted) return
         setError(null)
         setIsVoting(true)
         setVoteCount((currentVotes) => currentVotes + change)
@@ -18,6 +19,7 @@ function VoterBox ({article})
         try 
             {
                 await patchArticleVotes(article.article_id , change)
+                setHasVoted(true)
             } 
             catch (err) 
                 {
@@ -34,11 +36,11 @@ function VoterBox ({article})
 
         <div className="article-voter-box">
             
-            <button className="vote-btn" type="button" disabled={isVoting} onClick={()=>handleVote(-1)}>  - VOTE  </button>
+            <button className="vote-minus-btn" type="button" disabled={isVoting} onClick={()=>handleVote(-1)}>  - VOTE  </button>
             
             <p>  Votes: {voteCount}  </p>
             
-            <button className="vote-btn" type="button" disabled={isVoting} onClick={()=>handleVote(+1)}>  + VOTE  </button>
+            <button className="vote-plus-btn" type="button" disabled={isVoting} onClick={()=>handleVote(+1)}>  + VOTE  </button>
 
             {error && <p className="vote-error">{error}</p>}
 
