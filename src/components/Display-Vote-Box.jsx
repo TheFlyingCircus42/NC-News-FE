41a7860eacdcddf6d76bc0cd2774f9f2850fde79
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-26 13:27:40 +0000

    add vote counter to voter box component - not connected to api - no limit logic - wip

--- src/components/Display-Vote-Box.jsx
+++ src/components/Display-Vote-Box.jsx
@@ -1,13 +1,31 @@
 
+import { useState , useEffect } from "react"
 
 function VoterBox ({article}) 
 {
+    const [voteCount , setVoteCount] = useState(article.votes)
+
+    function increaseVote () 
+    {
+        setVoteCount((currentVotes) => currentVotes + 1)
+    }
+
+    function decreaseVote()
+    {
+        setVoteCount((currentVotes) => currentVotes -1)
+    }
+
+
     return(
 
     <div className="article-voter-box">
-            <button className="vote-btn" type="button">  - VOTE  </button>
-            <p> | Votes: {article.votes} | </p>
-            <button className="vote-btn" type="button">  + VOTE  </button>
+            
+            <button className="vote-btn" type="button" onClick={decreaseVote}>  - VOTE  </button>
+            
+            <p>  Votes: {voteCount}  </p>
+            
+            <button className="vote-btn" type="button" onClick={increaseVote}>  + VOTE  </button>
+
         </div>
     )
 }
