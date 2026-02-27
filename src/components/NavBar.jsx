96d76397ce272a8bddc314ac0e57245ca90beeca
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-02-27 13:47:10 +0000

    add drop down menu to all topics in nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,28 @@
 import '../styles/NavBar.css'
-// import { useState , useEffect } from 'react'
+import { useState , useEffect } from 'react'
 import { Link } from "react-router-dom"
 
 function NavBar () {
+    
+    const [showTopics , setShowTopics]=useState("false")
+
+    
+    function dropMenu (){
+        setShowTopics(!showTopics)
+    }
+    
     return(<>
     <nav className="Nav-Bar">
         
+    <div className='Nav-Bar-Links'>
         <Link to="/home">
         <p className="Nav-Bar-Link"> Home </p>
         </Link>
 
-        <Link to="/api/topics">
+        {/* <Link to="/api/topics">
         <p className="Nav-Bar-Link"> All Topics </p>
-        </Link>
+        </Link> */}
+        <button onClick={()=> dropMenu()}>All Topics</button>
 
         <Link to="/api/articles">
         <p className="Nav-Bar-Link"> All Articles </p>
@@ -21,9 +31,19 @@ function NavBar () {
         <Link to="/api">
         <p className="Nav-Bar-Link"> API Info </p>
         </Link>
+    </div>
+            
 
+            {showTopics && 
+        <div className='Nav-Bar-Topics-Drop'>
+            <Link to="/topics/coding">Coding</Link>
+            <Link to="/topics/football">Football</Link>
+            <Link to="topics/cooking">cooking</Link>
+            
+        </div> }
+    
         
-        {/* <p className="Nav-Bar-Link"> ??? </p> */}
+        
 
     </nav>
     
