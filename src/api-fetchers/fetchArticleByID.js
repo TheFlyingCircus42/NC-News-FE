416c96c5577715e352b1dd45c735b3eaca12fb08
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-16 16:44:34 +0100

    remove comments and console logs

--- src/api-fetchers/fetchArticleByID.js
+++ src/api-fetchers/fetchArticleByID.js
@@ -1,17 +1,10 @@
-// fetchArticleByID.js
-// fetch an articl by an id no
-
 async function fetchArticleByID (id){
 
     const response = await fetch(`https://nc-news-backend-wbzi.onrender.com/api/articles/${id}`);
-
     if(!response.ok) throw new Error("Cannot find articles list");
-
     const article = await response.json()
-    
+   
     return article
 }
-
-console.log("fetch by id" ,fetchArticleByID(2))
 
 export default fetchArticleByID
