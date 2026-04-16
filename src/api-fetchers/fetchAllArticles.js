46443e9aeefea9195f927facb8ba7baecf49fe98
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-16 16:43:40 +0100

    remove old code and comments

--- src/api-fetchers/fetchAllArticles.js
+++ src/api-fetchers/fetchAllArticles.js
@@ -1,18 +1,11 @@
 
 async function fetchAllArticles () {
-
     
-    const response = await fetch('https://nc-news-backend-wbzi.onrender.com/api/articles');
-
-        
+    const response = await fetch('https://nc-news-backend-wbzi.onrender.com/api/articles');     
     if(!response.ok) throw new Error("Cannot find articles list");
-
     const allArticles = await response.json()
     
     return allArticles
-    
 }
-
-console.log("fetchallarticles.js", fetchAllArticles())
 
 export default fetchAllArticles
