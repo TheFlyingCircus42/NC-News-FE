2deee52490c8906f2fed657a0c847fa1655fd1f7
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-19 16:44:18 +0100

    ammend import path for article cards css file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import NavBar from './components/NavBar'
 import AllArticles from './components/AllArticles'
-import '../src/styles/Article-Cards.css'
+import './styles/Article-Cards.css'
 import Home from './components/Home'
 import ArticleByID from './components/ArticleByID'
 import { Routes, Route } from "react-router-dom";
