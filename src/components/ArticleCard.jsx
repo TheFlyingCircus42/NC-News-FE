dd8c05f6b4ce2ec3916ed60dc19a18fc2b1e885f
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-22 09:27:40 +0100

    destructure article object

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,25 +5,38 @@ import { Link } from 'react-router-dom'
 
 function ArticleCard ({ article })
 {
-
-    if (!article) return null; 
     
-    const id = article.article_id
+    if (!article) return null; 
+    console.log(article)
+
+    const {
+        article_id,
+        article_img_url,
+        author,
+        comment_count,
+        created_at,
+        title,
+        topic,
+        votes
+    } = article;
+
+
+    // const id = article.article_id
 
     return(
     <div className="article-card">
         
-        <Link to={`/api/articles/${id}`}><h3 className="article-card-title">{article.title}</h3></Link>
+        <Link to={`/api/articles/${article_id}`}><h3 className="article-card-title">{title}</h3></Link>
 
         <div className="article-card-content">
 
-            <img className="article-card-img" src = {article.article_img_url} alt={article.title}/>
+            <img className="article-card-img" src = {article_img_url} alt={title}/>
             <p className="article-card-text">A bunch of place holder text, probably just the first few lines of the article body but I need to go into the backend to change that</p>
         </div>
 
         <div className="article-card-footer">
 
-            <p className="article-card-footer-content"> By {article.author} | Comments: {article.comment_count} | Votes: {article.votes} </p>
+            <p className="article-card-footer-content"> By {author} | Comments: {comment_count} | Votes: {votes} </p>
         </div>
         
       
