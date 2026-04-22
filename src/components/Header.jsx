e810d04b5827e2d7db0a6d83c68e5273619d0e07
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-22 13:28:52 +0100

    add link to home on logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import '../styles/Header.css'
+import {Link} from "react-router-dom"
 import logo from '../assets/NCLogo.png'
 
 
@@ -7,8 +8,10 @@ function Header ()
     
     <header className="Header">
     
-    {/* <p className="Header-Logo">Logo</p>     */}
-    <img src={logo} alt="NC News Logo" className='Header-Logo'></img>
+    <Link to="/">
+        <img src={logo} alt="NC News Logo" className='Header-Logo'></img>
+    </Link>
+
     <h1 className="Header-Title">NC NEWS</h1>
     <div className="Header-UserBox">
         <p className="Header-UserBox-Avtr">Avatar</p>
