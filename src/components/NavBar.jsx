b059922c66d05c30031da9c94b05debbdadc1fde
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-22 15:14:49 +0100

    refactor navbar code layout and class names

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,31 +7,37 @@ function NavBar () {
     const [showTopics , setShowTopics]=useState(false)
 
     
-    function dropMenu ()
-    {
-        setShowTopics(!showTopics)
-    }
+    // function dropMenu ()
+    // {
+    //     setShowTopics(!showTopics)
+    // }
     
     return(
     
         <nav className="Nav-Bar">
         
-            <div className='Nav-Bar-Links'>
-                <Link to="/home" className='nav-link'> Home </Link>           
+            <div className='Nav-Links'>
+
+                <Link to="/home" className='nav-link'> Home </Link>    
+
+                <div className='Nav-Dropdown'>
+                    <button className='Nav-Link'
+                    onClick={()=> setShowTopics(!showTopics)}>
+                        Topics ▾
+                    </button>
+                        {showTopics && 
+                            <div className='Nav-Bar-Topics-Drop'>
+                                <Link to="/topics/alltopics">All Topics</Link>
+                                <Link to="/topics/coding">Coding</Link>
+                                <Link to="/topics/football">Football</Link>
+                                <Link to="topics/cooking">Cooking</Link>  
+                            </div> }
+                </div>
+
                 <Link to="/articles" className='nav-link'> All Articles </Link>      
-                <Link to="/api" className='nav-link'> API </Link>
-                <button onClick={()=> dropMenu()}>Topics</button>
+                <Link to="/api" className='nav-link'> API Info </Link>  
             </div>
             
-            {/* TOPICS DROP DOWN MENU  */}
-                {showTopics && 
-            <div className='Nav-Bar-Topics-Drop'>
-                <Link to="/topics/alltopics">All Topics</Link>
-                <Link to="/topics/coding">Coding</Link>
-                <Link to="/topics/football">Football</Link>
-                <Link to="topics/cooking">Cooking</Link>  
-            </div> }
-    
         </nav>
     
     )
