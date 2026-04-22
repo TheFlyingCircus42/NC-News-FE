6886fe8f3fa40e9f99e48a1fab36a643fc490645
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-22 14:40:59 +0100

    tidy up code spacing in header jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,25 @@ import '../styles/Header.css'
 import {Link} from "react-router-dom"
 import logo from '../assets/NCLogo.png'
 
-
 function Header () 
-{ return(<>
+{ return(
     
     <header className="Header">
-    
+        <Link to="/" className='Header-left'>
+            <img src={logo} alt="NC News Logo" className='Header-Logo'></img>
+            <h1 className="Header-Title">NC <span>NEWS</span></h1>
+        </Link>
 
-    <Link to="/" className='Header-left'>
+        <div className="Header-UserBox">
+            <img src="https://i.pravatar.cc/40"
+                alt="User Avatar"
+                className='User-Avatar' />       
 
-    
-        <img src={logo} alt="NC News Logo" className='Header-Logo'></img>
-    
-    <h1 className="Header-Title">NC <span>NEWS</span></h1>
-    </Link>
-
-    <div className="Header-UserBox">
-        {/* <p className="Header-UserBox-Avtr">Avatar</p>
-         */}
-        <img src="https://i.pravatar.cc/40"
-        alt="User Avatar"
-        className='User-Avatar' />       
-
-        <span className='User-name'> Username </span>
-        <span className='User-Chevron'> ▾ </span>
-
+            <span className='User-name'> Username </span>
+            <span className='User-Chevron'> ▾ </span>
         </div>
-    </header>
-    
-    
-    </>)
-}
+    </header> 
 
+)}
 
 export default Header
