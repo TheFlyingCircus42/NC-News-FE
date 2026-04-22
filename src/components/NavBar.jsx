3b2c7eb3921ab8b82f5199c3d628718b61e0f595
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-22 17:08:07 +0100

    manually re-apply code changes for nav bar jsx and css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,84 @@
 import '../styles/NavBar.css'
-import { useState , useEffect } from 'react'
 import { Link } from "react-router-dom"
 
-function NavBar () {
-    
-    const [showTopics , setShowTopics]=useState(false)
+function NavBar() {
+  return (
+    <nav className="nav-bar">
 
-    
-    function dropMenu ()
-    {
-        setShowTopics(!showTopics)
-    }
-    
-    return(
-    
-        <nav className="Nav-Bar">
-        
-            <div className='Nav-Bar-Links'>
-                <Link to="/home" className='nav-link'> Home </Link>           
-                <Link to="/articles" className='nav-link'> All Articles </Link>      
-                <Link to="/api" className='nav-link'> API </Link>
-                <button onClick={()=> dropMenu()}>Topics</button>
-            </div>
-            
-            {/* TOPICS DROP DOWN MENU  */}
-                {showTopics && 
-            <div className='Nav-Bar-Topics-Drop'>
-                <Link to="/topics/alltopics">All Topics</Link>
-                <Link to="/topics/coding">Coding</Link>
-                <Link to="/topics/football">Football</Link>
-                <Link to="topics/cooking">Cooking</Link>  
-            </div> }
-    
-        </nav>
-    
-    )
+      <div className="nav-links">
+
+        <Link to="/home" className="nav-link">
+          Home
+        </Link>
+
+        <Link to="/articles" className="nav-link">
+          All Articles
+        </Link>
+
+        <div className="nav-dropdown">
+
+          <button className="nav-link nav-button">
+            Topics ▾
+          </button>
+
+          <div className="dropdown-menu">
+            <Link to="/topics/alltopics">All Topics</Link>
+            <Link to="/topics/coding">Coding</Link>
+            <Link to="/topics/football">Football</Link>
+            <Link to="/topics/cooking">Cooking</Link>
+          </div>
+
+        </div>
+
+        <Link to="/api" className="nav-link">
+          API Info
+        </Link>
+
+      </div>
+
+    </nav>
+  )
 }
 
 export default NavBar
+
+// import '../styles/NavBar.css'
+// import { useState , useEffect } from 'react'
+// import { Link } from "react-router-dom"
+
+// function NavBar () {
+    
+//     const [showTopics , setShowTopics]=useState(false)
+
+    
+//     function dropMenu ()
+//     {
+//         setShowTopics(!showTopics)
+//     }
+    
+//     return(
+    
+//         <nav className="Nav-Bar">
+        
+//             <div className='Nav-Bar-Links'>
+//                 <Link to="/home" className='nav-link'> Home </Link>           
+//                 <Link to="/articles" className='nav-link'> All Articles </Link>      
+//                 <Link to="/api" className='nav-link'> API </Link>
+//                 <button onClick={()=> dropMenu()}>Topics</button>
+//             </div>
+            
+//             {/* TOPICS DROP DOWN MENU  */}
+//                 {showTopics && 
+//             <div className='Nav-Bar-Topics-Drop'>
+//                 <Link to="/topics/alltopics">All Topics</Link>
+//                 <Link to="/topics/coding">Coding</Link>
+//                 <Link to="/topics/football">Football</Link>
+//                 <Link to="topics/cooking">Cooking</Link>  
+//             </div> }
+    
+//         </nav>
+    
+//     )
+// }
+
+// export default NavBar
