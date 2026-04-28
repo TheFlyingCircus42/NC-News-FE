c412fb4c7a4cd6f68c5f0e9923eef9fa95ababb7
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-28 15:48:00 +0100

    add trending articles and state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,26 @@ import '../styles/Home.css'
 import {useEffect, useState} from "react"
 import {Link} from "react-router-dom"
 import fetchAllArticles from '../api-fetchers/fetchAllArticles'
+import ArticleCard from './ArticleCard'
 
 function Home (){
 
     const [featured, setFeatured]=useState(null)
+    const [trending, setTrending]=useState(null)
 
     useEffect(()=>
         {
             fetchAllArticles().then((data)=>
                 {
                     const articles = data.articles
+                    
                     const random = articles[Math.floor(Math.random() * articles.length)]
                     setFeatured(random)
+
+                    const trendingArticles = [...articles]
+                        .sort((a, b) => b.votes - a.votes)
+                        .slice(0,4)
+                    setTrending(trendingArticles)
                 })
         }, [])
 
@@ -22,6 +30,7 @@ function Home (){
     return(
        <div className='home-container'>
 
+            <div className='home-featured-container'>
             <Link to={`/articles/${featured.article_id}`}
                 className='home-featured'>
 
@@ -39,8 +48,16 @@ function Home (){
                         <p className='home-featured-topic'>{featured.topic}</p>
                         <h2 className='home-featured-title'>{featured.title}</h2>
                 </div>
-
             </Link>
+            </div>
+
+            <div className='home-trending-row'>
+                {trending.map((article, i)=>(
+                    <div className={`trending-card ${i === 0 ? "featured" : ""}`}>
+                        <ArticleCard article={article}/>
+                    </div>
+                ))}
+            </div>
 
        </div>
     )
