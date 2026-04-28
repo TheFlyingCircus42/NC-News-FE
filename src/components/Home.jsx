20f70ef8836b5ad5a57df61dbd5ec70a5769da28
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-28 11:55:11 +0100

    generate random article to feature on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 import '../styles/Home.css'
+import {useEffect, useState} from "react"
+import {Link} from "react-router-dom"
+import fetchAllArticles from '../api-fetchers/fetchAllArticles'
 
-function Home ()
-{return(<>
-    <h2> WELCOME HOME!</h2>
+function Home (){
 
+    const [featured, setFeatured]=useState(null)
 
-</>)}
+    useEffect(()=>
+        {
+            fetchAllArticles().then((data)=>
+                {
+                    const articles = data.articles
+                    const random = articles[Math.floor(Math.random() * articles.length)]
+                    setFeatured(random)
+                    
+
+                })
+
+                    console.log("FEATURE:", featured)
+
+        }, [])
+
+        if (!featured) return
+
+    return(
+        <p>random title</p>
+    )
+}
 
 export default Home
