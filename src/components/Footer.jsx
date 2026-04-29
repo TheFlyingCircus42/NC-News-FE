64e5dc9ed0aa3b2bb41855ef73f4cc6e1c0ce1dd
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-29 09:17:50 +0100

    re organis footer and add links out

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,21 @@ import '../styles/Footer.css'
 function Footer ()
 {
     return(
-        <div className="Footer">
-    <p>By Rob Hawkins</p>
-    <p>At Northcoders</p>
-    <p>2026</p>
-    </div>)
+        <footer className='footer'>
+            <div className='footer-inner'>
+
+                <div className='footer-byLine'>
+                    <p className='footer-name'>Robert Hawkins</p>
+                    <p className='footer-meta'> Built at Northcoderds · 2026</p>
+                </div>
+
+                <div className='footer-links'>
+                    <a href="https://github.com/TheFlyingCircus42">GitHub</a>
+                    <a href='www.linkedin.com/in/rob-hawkins-866a02331'>LinkedIn</a>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
