aea89b2adfc005eac92cf7fd752061834d537c5d
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-04-29 09:57:56 +0100

    add styling and content to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,45 @@
 import '../styles/Footer.css'
 
-function Footer ()
-{
-    return(
-        <footer className='footer'>
-            <div className='footer-inner'>
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-inner">
 
-                <div className='footer-byLine'>
-                    <p className='footer-name'>Robert Hawkins</p>
-                    <p className='footer-meta'> Built at Northcoderds · 2026</p>
-                </div>
 
-                <div className='footer-links'>
-                    <a href="https://github.com/TheFlyingCircus42">GitHub</a>
-                    <a href='www.linkedin.com/in/rob-hawkins-866a02331'>LinkedIn</a>
-                </div>
-            </div>
-        </footer>
-    )
+        <div className="footer-left">
+          <p className="footer-name">Robert Hawkins</p>
+          
+          <p className="footer-meta">
+            Built at Northcoders · 2026
+          </p>
+
+          <div className="footer-links">
+            <a href="https://github.com/TheFlyingCircus42">GitHub</a>
+            <span>·</span>
+            <a href="https://www.linkedin.com/in/rob-hawkins-866a02331">LinkedIn</a>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          {/* <p className="footer-meta footer-stack-text">
+            React · Node.js · Express · PostgreSQL · REST API
+          </p> */}
+           <p className="footer-meta footer-stack-text">
+            NC News is a fullstsack create, write, update, delete demo app. 
+           </p>
+
+          <div className="tech-stack">
+            <span className="tech-badge">React</span>
+            <span className="tech-badge">Node</span>
+            <span className="tech-badge">Express</span>
+            <span className="tech-badge">PostgreSQL</span>
+            <span className="tech-badge">API</span>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
