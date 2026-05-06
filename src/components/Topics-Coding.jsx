a8a5c82a2d62c53d0f5687a912be3d0033a52002
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-05-06 15:40:32 +0100

    ass styling to section description

--- src/components/Topics-Coding.jsx
+++ src/components/Topics-Coding.jsx
@@ -1,19 +1,21 @@
 import AllArticles from "./AllArticles"
 import ArticlesByTopic from "./Articles-By-Topic"
+import '../styles/Topics-Page.css'
 
 function TopicsCoding () 
 {
 
     const topic = "coding"
     return (<>
-    <h3> Coding </h3>
-    <p> Learn everything about coding - it's like The Matrix but less dangerous. </p>
-    
+    <div className="single-topic-container">
+    <h3 className="section-title"> Coding </h3>
+    <p className="topic-description"> Learn everything about coding - it's like The Matrix but less dangerous. </p>
+     </div>
     <ArticlesByTopic topic={topic}></ArticlesByTopic>
-
+       
+    </>
     
-    
-    </>)
+    )
 
 
 }
